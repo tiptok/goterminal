6292a47473569a3ced14427270d2f0dd3b69fa9b
yangfu <785409885@qq.com>
2018-10-30 15:53:45 +0800

    测试IP

--- main.go
+++ main.go
@@ -3,8 +3,7 @@ package main
 func main() {
 	var exit chan int
 	exit = make(chan int)
-	t := NewTerminal("218.5.10.82", 38922)
-	//t := NewTerminal("127.0.0.1", 38922)
+	t := NewTerminal("127.0.0.1", 38922)
 	if t != nil {
 		go t.KeepConn()
 	}
